1e1a5f7e28baab084b24d19e762d9e01bb9749cf
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2022-06-21 20:40:31 -0300

    criado usecase de criação de usuario

--- src/modules/users/useCases/createUser/CreateUserController.ts
+++ src/modules/users/useCases/createUser/CreateUserController.ts
@@ -6,7 +6,13 @@ class CreateUserController {
   constructor(private createUserUseCase: CreateUserUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { email, name } = request.body;
+      this.createUserUseCase.execute({ email, name });
+      return response.status(201).send();
+    } catch (err) {
+      return response.status(400).json(err);
+    }
   }
 }
 

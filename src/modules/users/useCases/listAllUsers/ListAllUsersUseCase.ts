e2ae57e8a39b0d333f128fb34e2c225f55bafc3c
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2022-06-21 22:20:54 -0300

    desafio finalizado com sucesso, o pai é brabo

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -11,13 +11,7 @@ class ListAllUsersUseCase {
   execute({ user_id }: IRequest): User[] {
     const user = this.usersRepository.findById(user_id);
 
-    if (!user) {
-      throw new Error("Mensagem do erro");
-    }
-
-    const isUserAdmin = user.admin === true;
-
-    if (!isUserAdmin) {
+    if (!user.admin) {
       throw new Error("Mensagem do erro");
     }
     const allUsers = this.usersRepository.list();
